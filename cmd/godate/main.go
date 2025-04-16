73683b24b61fc0944db81d495da02d3e19b92dd5
Edditoria <edditoria@pm.me>
2025-04-16 15:40:26 +0800

    godate: Add quick commands: utc, strict and serial

--- cmd/godate/main.go
+++ cmd/godate/main.go
@@ -16,6 +16,7 @@ import (
 type presetFormat struct {
 	Layout  string // in Go time format.
 	IsLocal bool   // to show local time or UTC time.
+	Usage   string // One-line usage.
 }
 
 // @implement [fmt.Stringer].
@@ -28,11 +29,12 @@ func (p presetFormat) String() string {
 
 // Collection of preset format.
 var (
-	preDateOnly         = presetFormat{"2006-01-02", true}
-	preTimeOnly         = presetFormat{"15:04:05", true}
-	preDateTimeFriendly = presetFormat{"2006-01-02 15:04:05 UTC-07", true}
-	preDateTimeSerial   = presetFormat{"2006-01-02-150405", true}
-	preDateTimeUTC      = presetFormat{time.RFC3339, false}
+	preDateTimeFriendly = presetFormat{"2006-01-02 15:04:05 Mon UTC-07", true, "friendly local date-time"}
+	preDateTimeUTC      = presetFormat{time.RFC3339, false, "RFC3339-compatible UTC date-time"}
+	preDateTimeStrict   = presetFormat{time.RFC3339, true, "RFC3339-compatible local date-time"}
+	preDateTimeSerial   = presetFormat{"2006-01-02-150405", true, "serial-like local date-time"}
+	preDateOnly         = presetFormat{"2006-01-02", true, "local date without time"}
+	preTimeOnly         = presetFormat{"15:04:05", true, "local time without date"}
 )
 
 var (
@@ -64,6 +66,15 @@ func main() {
 
 	// When cli: godate [action] [-flag...]
 	switch os.Args[1] {
+	case "utc":
+		exitCode := handleQuickCommand("utc", &preDateTimeUTC)
+		os.Exit(exitCode)
+	case "strict":
+		exitCode := handleQuickCommand("strict", &preDateTimeStrict)
+		os.Exit(exitCode)
+	case "serial":
+		exitCode := handleQuickCommand("serial", &preDateTimeSerial)
+		os.Exit(exitCode)
 	default:
 		// When cli: godate [-flag...] or godate wrongArg(s)
 		flag.Parse()
@@ -88,6 +99,21 @@ func main() {
 		fmt.Fprintf(os.Stdout, "%s\n", presetOpt)
 		os.Exit(0)
 	}
+}
+
+// It will print messages in console.
+//   - @return Exit code.
+func handleQuickCommand(name string, preFmt *presetFormat) int {
+	if len(os.Args) == 3 && (os.Args[2] == "-h" || os.Args[2] == "--help") {
+		fmt.Fprintf(os.Stdout, "Print %s\n", preFmt.Usage)
+		return 0
+	}
+	if len(os.Args) > 2 {
+		fmt.Fprintf(os.Stderr, "godate: %s does not take argument\nSee: godate %s -h\n", name, name)
+		return 2
+	}
+	fmt.Fprintf(os.Stdout, "%s\n", preFmt)
+	return 0
 }
 
 func handlePresetFlag() (*presetFormat, error) {
