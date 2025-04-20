29327539245f00a34bb9ca797d5df956427a0063
Edditoria <edditoria@pm.me>
2025-04-20 19:43:09 +0800

    flags: Add SubcmdGroup with SubcmdGroup.Help

--- flags/subcmd.go
+++ flags/subcmd.go
@@ -5,6 +5,58 @@ import (
 	"strings"
 )
 
+// Subcommand group for building help message.
+type SubcmdGroup struct {
+	Name       string // Group name.
+	Summary    string // Short description for main help.
+	SubcmdList []*Subcmd
+}
+
+// Who's the longest?
+// If the list is empty, it returns 0.
+func (group *SubcmdGroup) LongestSubcmdName() int {
+	longest := 0
+	if len(group.SubcmdList) == 0 {
+		return 0
+	}
+	for _, subcmd := range group.SubcmdList {
+		thisSize := len(subcmd.FlagSet.Name())
+		if thisSize > longest {
+			longest = thisSize
+		}
+	}
+	return longest
+}
+
+// Generate help message of the [SubcmdGroup] session.
+// It is a part of full [PrintHelp].
+// The message will look like:
+//
+//	[Name]: [Heading to brief]
+//
+//	  [subcmd1]  [subcmd description]
+//	  [subcmd2]  [subcmd description]
+//	  [subcmd3]  [subcmd description]
+//	  (and so on...)
+//
+// For efficiency, this function takes a [strings.Builder].
+// You can "continue" to build string in other functions.
+// When everything ready, do `sb.String()` to build the string.
+//
+// The output ends with a new-line character.
+func (group *SubcmdGroup) Help(sb *strings.Builder) {
+	sb.WriteString(group.Name)
+	sb.WriteString(": ")
+	sb.WriteString(group.Summary)
+	sb.WriteString("\n\n")
+	longest := group.LongestSubcmdName()
+	for _, subcmd := range group.SubcmdList {
+		sb.WriteString(subcmd.HelpLine(2, longest))
+		sb.WriteByte('\n')
+	}
+	sb.WriteByte('\n')
+}
+
 // Subcommand to expand [flag.FlagSet] features.
 // Recommend to start with [NewSubcmd] function.
 type Subcmd struct {
